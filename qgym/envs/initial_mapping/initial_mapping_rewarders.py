5687f6f112e2e4b36357fe43a55f487bea8f654d
Tariq Bontekoe <tariq.bontekoe@tno.nl>
2022-08-10 14:25:31 +0200

    simplify initial mapping rewarders

--- qgym/envs/initial_mapping/initial_mapping_rewarders.py
+++ qgym/envs/initial_mapping/initial_mapping_rewarders.py
@@ -5,11 +5,10 @@ OpenQL.
 
 from __future__ import annotations
 
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Optional
 
 import numpy as np
 from numpy.typing import NDArray
-from scipy.sparse import csr_matrix
 
 from qgym import Rewarder
 
@@ -56,41 +55,19 @@ class BasicRewarder(Rewarder):
         if self._is_illegal(action, old_state):
             return self._illegal_action_penalty
 
-        mapped_edges = self._get_mapped_edges(new_state)
+        return self._compute_state_reward(new_state)
 
+    def _compute_state_reward(self, state: Dict[Any, Any]) -> float:
         reward = 0.0
-        for i, j in mapped_edges:
-            if new_state["connection_graph_matrix"][i, j] == 0:
+        for interaction_i, interaction_j in zip(*state["interaction_graph_matrix"].nonzero()):
+            mapped_interaction_i = state["mapping_dict"][interaction_i]
+            mapped_interaction_j = state["mapping_dict"][interaction_j]
+            if state["connection_graph_matrix"][mapped_interaction_i][mapped_interaction_j] == 0:
                 reward += self._penalty_per_edge
             else:
                 reward += self._reward_per_edge
 
         return reward
-
-    def _get_mapped_edges(self, new_state: Dict[Any, Any]) -> list[list[int, int]]:
-        """
-        Computes a list of mapped edges of the new state
-
-        :param new_state: Updated state of the InitialMapping
-        """
-        mapped_edges = []
-
-        for logical_qubit_idx, physical_qubit_idx in new_state["mapping_dict"].items():
-            neighbours = self._get_neighbours(
-                logical_qubit_idx, new_state["interaction_graph_matrix"]
-            )
-
-            mapped_neighbours = neighbours & new_state["logical_qubits_mapped"]
-
-            for mapped_neighbour in mapped_neighbours:
-                mapped_edges.append(
-                    [
-                        physical_qubit_idx,
-                        new_state["mapping_dict"][mapped_neighbour],
-                    ]
-                )
-
-        return mapped_edges
 
     @staticmethod
     def _is_illegal(action: NDArray[np.int_], old_state: Dict[Any, Any]) -> bool:
@@ -105,32 +82,6 @@ class BasicRewarder(Rewarder):
             action[0] in old_state["physical_qubits_mapped"]
             or action[1] in old_state["logical_qubits_mapped"]
         )
-
-    def _get_neighbours(self, qubit_idx: int, adjacency_matrix: csr_matrix) -> set:
-        """
-        Computes a set of neighbours of a given qubit (i.e., a set of nodes with which
-        this node has a connection.)
-
-        :param qubit_idx: index of the qubit of which the neighbours are computed.
-        :param adjacency_matrix: adjacency matrix of a graph
-        """
-        neighbours = set()
-        for neighbour_idx in range(adjacency_matrix.shape[0]):
-            if self._are_neighbours((qubit_idx, neighbour_idx), adjacency_matrix):
-                neighbours.add(neighbour_idx)
-        return neighbours
-
-    @staticmethod
-    def _are_neighbours(
-        qubit_idxs: Tuple[int, int], adjacency_matrix: csr_matrix
-    ) -> bool:
-        """
-        Checks if two qubits are neighbours
-
-        :param qubit_idxs: indexes of the qubits to check
-        :param adjacency_matrix: adjacency matrix of a graph
-        """
-        return adjacency_matrix[qubit_idxs[0], qubit_idxs[1]] != 0
 
 
 class SingleStepRewarder(BasicRewarder):
@@ -158,24 +109,7 @@ class SingleStepRewarder(BasicRewarder):
         if self._is_illegal(action, old_state):
             return self._illegal_action_penalty
 
-        old_cumulative_reward = self.compute_cumulative_reward(old_state)
-        new_cumulative_reward = self.compute_cumulative_reward(new_state)
-
-        reward = new_cumulative_reward - old_cumulative_reward
-
-        return reward
-
-    def compute_cumulative_reward(self, state: Dict[Any, Any]):
-        mapped_edges = self._get_mapped_edges(state)
-
-        reward = 0.0
-        for i, j in mapped_edges:
-            if state["connection_graph_matrix"][i, j] == 0:
-                reward += self._penalty_per_edge
-            else:
-                reward += self._reward_per_edge
-
-        return reward
+        return self._compute_state_reward(new_state) - self._compute_state_reward(old_state)
 
 
 class EpisodeRewarder(BasicRewarder):
@@ -209,6 +143,4 @@ class EpisodeRewarder(BasicRewarder):
         ):
             return 0
 
-        return super().compute_reward(
-            old_state=old_state, action=action, new_state=new_state
-        )
+        return self._compute_state_reward(new_state)
