3ca02e7549a8f04093f6eb2d207cb2c1538c4d30
Willem de kok <willem.dekok@tno.nl>
2023-03-15 12:04:37 +0100

    first update of doc-string routing module.

--- qgym/envs/routing/routing.py
+++ qgym/envs/routing/routing.py
@@ -1,1 +1,266 @@
-#placeholder
+r"""This module contains an environment for training an RL agent on the routing
+problem of OpenQL. The routing problem is aimed at enabling to execute the quantum circuit 
+by putting those physical qubits into connection that have an interaction in the quantum
+circuit. This problem arises when there are mismatches between the interaction graph and the
+QPU-topology in the initial mapping.
+The quantum circuit is represented as an **interaction graph**, where each node represent a 
+qubit and each edge represent an interaction between two qubits as defined by the circuit 
+(See the example below). The QPU structure is called the **connection graph**. In the connection
+graph each node represents a physical qubit and each edge represent a connection between
+two qubits in the QPU.
+
+
+.. code-block:: console
+
+              QUANTUM CIRCUIT                        INTERACTION GRAPH
+           ┌───┐               ┌───┐
+    |q3>───┤ R ├───┬───────────┤ M ╞══                 q1 ────── q2
+           └───┘   │           └───┘                            ╱
+           ┌───┐ ┌─┴─┐         ┌───┐                           ╱
+    |q2>───┤ R ├─┤ X ├───┬─────┤ M ╞══                        ╱
+           └───┘ └───┘   │     └───┘                         ╱
+           ┌───┐       ┌─┴─┐   ┌───┐                        ╱
+    |q1>───┤ R ├───┬───┤ X ├───┤ M ╞══                     ╱
+           └───┘   │   └───┘   └───┘                      ╱
+           ┌───┐ ┌─┴─┐         ┌───┐                     ╱
+    |q0>───┤ R ├─┤ X ├─────────┤ M ╞══                q3 ─────── q4
+           └───┘ └───┘         └───┘
+
+
+A SWAP-gate changes the mapping from logical qubits to physical qubits at a certain point in 
+the circuit, and thereby allows to solve mismatchings from the initial mapping.
+The goal is to place SWAP-gates in the quantum circuit to fixed the mismatches. The least amount
+of SWAP-gates is preferred. In more advanced setups, also different factors can be taken into 
+account, like the fidelity of edge in the QPU etc.
+
+
+State Space:
+    The state space is described by a ``RoutingState`` with the following
+    attributes:
+
+    * `steps_done`: Number of steps done since the last reset.
+    * `num_nodes`: Number of *physical* qubits.
+    * `graphs`: Dictionary containing the graph and matrix representation of the connection graph.
+    * `mapping`: Array of which the index represents a physical qubit, and the value a
+      virtual qubit. A value of ``num_nodes + 1`` represents the case when nothing is
+      mapped to the physical qubit yet.
+    * `mapping_dict`: Dictionary that maps logical qubits (keys) to physical qubit
+      (values).
+    * `mapped_qubits`: Dictionary with a two Sets containing all mapped physical and
+      logical qubits.
+
+Observation Space:
+    The observation space is a ``qgym.spaces.Dict`` with 2 entries:
+
+    * `m_execution_array`: Array with Boolean values for the upcoming m multiple qubit gates in the quantum circuit. 
+      A Boolean value True in the array indicates that, given the current mapping and the connection graph, that 
+      multiple qubit gate can be executed.
+    * `mapping`: The current state of the mapping.
+
+Action Space:
+    A valid action is a tuple of integers  $(i,j,k)$, such that $i$ indicates where in
+    the circuit (i.e. after the i-th gate, counting from zero) to put in a SWAP-gate on 
+    physical qubits $j$ and $k$.
+    An action is legal when:
+    #. physical qubit $j$ does not equal physical qubit $k$.
+    #. physical qubits $j$ and $k$ have a connection between them in the connection graph.
+
+TODO: create Examples
+
+
+"""
+import warnings
+from copy import deepcopy
+from typing import Any, Dict, Iterable, List, Optional, Tuple, Union, cast
+
+import networkx as nx
+import numpy as np
+from networkx import Graph, grid_graph
+from numpy.typing import ArrayLike, NDArray
+
+import qgym.spaces
+from qgym.envs.initial_mapping.initial_mapping_rewarders import BasicRewarder
+from qgym.envs.initial_mapping.initial_mapping_state import InitialMappingState
+from qgym.envs.initial_mapping.initial_mapping_visualiser import (
+    InitialMappingVisualiser,
+)
+from qgym.templates import Environment, Rewarder
+from qgym.utils.input_validation import (
+    check_adjacency_matrix,
+    check_graph_is_valid_topology,
+    check_instance,
+    check_real,
+)
+
+Gridspecs = Union[List[Union[int, Iterable[int]]], Tuple[Union[int, Iterable[int]]]]
+
+
+class InitialMapping(Environment[Dict[str, NDArray[np.int_]], NDArray[np.int_]]):
+    """RL environment for the initial mapping problem of OpenQL."""
+
+    def __init__(
+        self,
+        interaction_graph_edge_probability: float,
+        *,
+        connection_graph: Optional[Graph] = None,
+        connection_graph_matrix: Optional[ArrayLike] = None,
+        connection_grid_size: Optional[Gridspecs] = None,
+        rewarder: Optional[Rewarder] = None,
+    ) -> None:
+        """Initialize the action space, observation space, and initial states.
+        Furthermore, the connection graph and edge probability for the random
+        interaction graph of each episode is defined.
+
+        The supported render modes of this environment are "human" and "rgb_array".
+
+        :param interaction_graph_edge_probability: Probability that an edge between any
+            pair of qubits in the random interaction graph exists. The interaction
+            graph will have the same amount of nodes as the connection graph. Nodes
+            without any interactions can be seen as 'null' nodes. Must be a value in the
+            range [0,1].
+        :param connection_graph: ``networkx`` graph representation of the QPU topology.
+            Each node represents a physical qubit and each node represents a connection
+            in the QPU topology.
+        :param connection_graph_matrix: Adjacency matrix representation of the QPU
+            topology.
+        :param connection_grid_size: Size of the connection graph when the connection
+            graph has a grid topology. For more information on the allowed values and
+            types, see ``networkx`` `grid_graph`_ documentation.
+        :param rewarder: Rewarder to use for the environment. Must inherit from
+            ``qgym.Rewarder``. If ``None`` (default), then ``BasicRewarder`` is used.
+
+        .. _grid_graph: https://networkx.org/documentation/stable/reference/generated/
+            networkx.generators.lattice.grid_graph.html#grid-graph
+        """
+        interaction_graph_edge_probability = check_real(
+            interaction_graph_edge_probability,
+            "interaction_graph_edge_probability",
+            l_bound=0,
+            u_bound=1,
+        )
+        connection_graph = self._parse_connection_graph(
+            connection_graph=connection_graph,
+            connection_graph_matrix=connection_graph_matrix,
+            connection_grid_size=connection_grid_size,
+        )
+
+        self._rewarder = self._parse_rewarder(rewarder)
+
+        # Define internal attributes
+        self._state = InitialMappingState(
+            connection_graph, interaction_graph_edge_probability
+        )
+        self.observation_space = self._state.create_observation_space()
+        # Define attributes defined in parent class
+        self.action_space = qgym.spaces.MultiDiscrete(
+            nvec=[self._state.num_nodes, self._state.num_nodes], rng=self.rng
+        )
+
+        self.metadata = {"render.modes": ["human", "rgb_array"]}
+
+        self._visualiser = InitialMappingVisualiser(connection_graph)
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        return_info: bool = False,
+        interaction_graph: Optional[Graph] = None,
+        **_kwargs: Any,
+    ) -> Union[
+        Dict[str, NDArray[np.int_]],
+        Tuple[Dict[str, NDArray[np.int_]], Dict[str, Any]],
+    ]:
+        """Reset the state and set a new interaction graph.
+
+        To be used after an episode is finished.
+
+        :param seed: Seed for the random number generator, should only be provided
+            (optionally) on the first reset call i.e., before any learning is done.
+        :param return_info: Whether to receive debugging info. Default is ``False``.
+        :param interaction_graph: Interaction graph to be used for the next iteration,
+            if ``None`` a random interaction graph will be created.
+        :param _kwargs: Additional options to configure the reset.
+        :return: Initial observation and optionally debugging info.
+        """
+        # call super method for dealing with the general stuff
+        return super().reset(
+            seed=seed, return_info=return_info, interaction_graph=interaction_graph
+        )
+
+    def add_random_edge_weights(self) -> None:
+        """Add random weights to the connection graph and interaction graph."""
+        cast(InitialMappingState, self._state).add_random_edge_weights()
+
+    @staticmethod
+    def _parse_connection_graph(
+        *,
+        connection_graph: Any,
+        connection_graph_matrix: Any,
+        connection_grid_size: Any,
+    ) -> Graph:
+        """Parse the user input (given in ``__init__``) to create a connection graph.
+
+        :param connection_graph: ``networkx.Graph`` representation of the QPU topology.
+        :param connection_graph_matrix: Adjacency matrix representation of the QPU
+            topology
+        :param connection_grid_size: Size of the connection graph when the topology is a
+            grid.
+        :raise ValueError: When `connection_graph`, `connection_graph_matrix` and
+            `connection_grid_size` are all None.
+        :return: Connection graph as a ``networkx.Graph``.
+        """
+        if connection_graph is not None:
+            if connection_graph_matrix is not None:
+                msg = "Both 'connection_graph' and 'connection_graph_matrix' were "
+                msg += "given. Using 'connection_graph'."
+                warnings.warn(msg)
+            if connection_grid_size is not None:
+                msg = "Both 'connection_graph' and 'connection_grid_size' were given. "
+                msg += "Using 'connection_graph'."
+                warnings.warn(msg)
+
+            check_graph_is_valid_topology(connection_graph, "connection_graph")
+
+            # deepcopy the graphs for safety
+            return deepcopy(connection_graph)
+
+        if connection_graph_matrix is not None:
+            if connection_grid_size is not None:
+                msg = "Both 'connection_graph_matrix' and 'connection_grid_size' were "
+                msg += "given. Using 'connection_graph_matrix'."
+                warnings.warn(msg)
+            return InitialMapping._parse_adjacency_matrix(connection_graph_matrix)
+        if connection_grid_size is not None:
+            # Generate connection grid graph
+            return grid_graph(connection_grid_size)
+
+        msg = "No valid arguments for instantiation of the initial mapping environment "
+        msg += "were provided."
+        raise ValueError(msg)
+
+    @staticmethod
+    def _parse_rewarder(rewarder: Union[Rewarder, None]) -> Rewarder:
+        """Parse the `rewarder` given by the user.
+
+        :param rewarder: ``Rewarder`` to use for the environment. If ``None``, then the
+            ``BasicRewarder`` with default settings is used.
+        :return: Rewarder.
+        """
+        if rewarder is None:
+            return BasicRewarder()
+        check_instance(rewarder, "rewarder", Rewarder)
+        return deepcopy(rewarder)
+
+    @staticmethod
+    def _parse_adjacency_matrix(connection_graph_matrix: ArrayLike) -> Graph:
+        """Parse a given connection graph adjacency matrix to its respective graph.
+
+        :param connection_graph_matrix: adjacency matrix representation of the QPU
+            topology.
+        :raise TypeError: When the provided matrix is not a valid adjacency matrix.
+        :return: Graph representation of the adjacency matrix.
+        """
+        connection_graph_matrix = check_adjacency_matrix(connection_graph_matrix)
+
+        return nx.from_numpy_array(connection_graph_matrix)
