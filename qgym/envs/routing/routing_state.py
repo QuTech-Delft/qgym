16a5303338915061a2d6c33f3e77607cdb11ed30
Willem de kok <willem.dekok@tno.nl>
2023-03-28 12:11:05 +0200

    routingstate progress

--- qgym/envs/routing/routing_state.py
+++ qgym/envs/routing/routing_state.py
@@ -6,7 +6,7 @@ This ``RoutingState``represents the ``State`` of the ``Routing`` environment.
 """
 from __future__ import annotations
 
-from typing import Any, Dict, List, Optional, Set, Union, cast
+from typing import Any, Dict, List, Optional, Set, Union, cast, Tuple
 
 import numpy as np
 from numpy.typing import NDArray
@@ -14,7 +14,7 @@ from numpy.typing import NDArray
 from qgym.custom_types import Gate
 
 import qgym.spaces
-from qgym.envs.routing.machine_properties import MachineProperties
+from qgym.envs.scheduling.machine_properties import MachineProperties
 from qgym.envs.routing.routing_dataclasses import (
     RoutingUtils,
     CircuitInfo,
@@ -37,15 +37,15 @@ class RoutingState(
     :ivar busy: Used internally for the hardware limitations.
     """
     
-    #TODO: how to initialize with a giving Initial_Mapping?
+    #TODO: how to initialize with a giving Initial_Mapping? NOT NEEDED in RoutingState
 
     def __init__(
         self,
         *,
         machine_properties: MachineProperties,
-        max_gates: int,
+        max_interaction_gates: int,
         random_circuit_mode: str,
-        max_observation_reach: int,
+        max_observation_depth: int,
     ) -> None:
         self.steps_done = 0
 
@@ -72,6 +72,9 @@ class RoutingState(
         #     dependencies=np.empty((dependency_depth, max_gates), dtype=int),
         # )
 
+        self.observation_depth = max_observation_depth
+        self.max_interaction_gates = max_interaction_gates
+        
         # Generate circuit only containing interaction gates
         #TODO: think about keeping track of where in the circuit the interaction gate are.
         self.interaction_circuit = [gate for gate in circuit if not gate.q1 == gate.q2]
@@ -84,29 +87,48 @@ class RoutingState(
         self._update_episode_constant_observations()
         self._update_legal_actions()
 
+    def reset(
+            self,
+            *,
+            seed: Optional[int] = None,
+            circuit: Optional[List[Gate]] = None,
+            **_kwargs: Any,
+        ) -> SchedulingState:
+            """Reset the state and load a new (random) initial state.
+
+            To be used after an episode is finished.
+
+            :param seed: Seed for the random number generator, should only be provided
+                (optionally) on the first reset call, i.e., before any learning is done.
+            :param circuit: Optional list of a circuit for the next episode, each entry in
+                the list should be a ``Gate``. When a circuit is give, no random circuit
+                will be generated.
+            :param _kwargs: Additional options to configure the reset.
+            :return: Self.
+            """
+    #TODO think about initial mapping with new circuit
+    
     def update_state(self, action : NDArray[np.int_]) -> RoutingState:
         """Update the state of this environment using the given action.
 
-        :param action: If True a random SWAP-gate will be placed before the 
-        first observed gate, if False the first observed gate will be 
-        surpassed
+        :param action: If action[0]==True a SWAP-gate applied to qubits action[1], 
+        action[2] will be placed before the first observed gate, if False the first 
+        observed gate will be surpassed.
         :return: Updated state.
         """
         # Increase the step number
         self.steps_done += 1
+        
+        #surpass current_gate
+        if not action[1]:
+            self.position += 1
+            return self
 
         # Insert random swap-gate
-        if action:
-            #TODO: SHOULD WE IMPLEMENT THIS OR IS THIS DONE BY THE AGENT?            
-            random_q1 = int(self.rng.integers(low=0, high=MachineProperties.n_qubits, size=0))
-            random_q2 = int(self.rng.integers(low=0, high=MachineProperties.n_qubits, size=0))
-            random_SWAP_gate = Gate('swap', q1=random_q1, q2=random_q2)               
-            self.swap_gates_inserted.append((self.position, random_SWAP_gate))
-            #TODO: update_mapping accordingly
-
-        else:
-            #surpass current_gate
-            self.position += 1
+        SWAP_gate_to_insert = (action[1], action[2])
+        if self._is_legal[SWAP_gate_to_insert]:
+            self._place_SWAP_gate(action[1], action[2])           
+        
         return self
 
     def obtain_observation(self) -> ObservationT:
@@ -127,7 +149,7 @@ class RoutingState(
         """:return: Optional debugging info for the current state."""
         raise NotImplementedError
 
-    def create_observation_space(self, max_observation_reach: int) -> Space:
+    def create_observation_space(self) -> Space:
         """Create the corresponding observation space.
         
         :returns: Observation space in the form of a ``qgym.spaces.Dict`` space
@@ -143,10 +165,20 @@ class RoutingState(
             * ``qgym.spaces.MultiDiscrete`` space representing the first $n$ gates
               that must be scheduled before this gate.
         """  
-        observation_reach = min(max_observation_reach, len(interaction_circuit)- position)
+        observation_reach = min(max_observation_reach, len(interaction_circuit)- self.position)
 
 
         observation_space = qgym.spaces.MultiDiscrete(
             np.full(2 * observation_reach, n_qubits + 1), rng=self.rng
         )
         return observation_space
+    
+    def _place_SWAP_gate(self, qubit1:int, qubit2:int) -> None:       
+        #TODO: from collections import DeQueue for more efficient storage
+        self.swap_gates_inserted.append((self.position, qubit1, qubit2))
+        
+        #TODO: update_mapping accordingly
+        self.update_mapping(qubit1, qubit2)
+        
+    def _is_legal(self, SWAP_gate: Tuple[int, int]):
+        #TODO:checks
