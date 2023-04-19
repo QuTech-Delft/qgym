089ae021ac28595e30785fdcc1e5c73725c18ca9
Stan van der Linde <stan.vanderlinde@tno.nl>
2023-04-19 14:06:11 +0200

    Split test functions

--- tests/envs/routing/test_routing_state.py
+++ tests/envs/routing/test_routing_state.py
@@ -1,15 +1,18 @@
+"""This module contains tests for the ``RoutingState`` class."""
+from typing import List, Tuple
+
 import networkx as nx
 import numpy as np
 import pytest
-from numpy.typing import NDArray
+from numpy.typing import ArrayLike, NDArray
 
 import qgym.spaces
 from qgym.envs.routing.routing_state import RoutingState
 
 
 # Arrange
-@pytest.fixture
-def quad_graph():
+@pytest.fixture(name="quad_graph", scope="class")
+def quad_graph_fixture() -> nx.Graph:
     quad_graph = nx.Graph()
     quad_graph.add_edge(0, 1)
     quad_graph.add_edge(1, 2)
@@ -18,13 +21,25 @@ def quad_graph():
     return quad_graph
 
 
+@pytest.fixture(name="simple_state")
+def simple_state_fixture(quad_graph: nx.Graph) -> RoutingState:
+    return RoutingState(
+        max_interaction_gates=10,
+        max_observation_reach=5,
+        connection_graph=quad_graph,
+        observation_booleans_flag=False,
+        observation_connection_flag=False,
+    )
+
+
 @pytest.mark.parametrize(
     "max_interaction_gates, max_observation_reach",
     [(1, 1), (5, 5), (10, 5), (50, 5), (5, 10)],
+    scope="class",
 )
-def test_routing_state_initialize(
-    max_interaction_gates, max_observation_reach, quad_graph
-):
+def test_init(
+    max_interaction_gates: int, max_observation_reach: int, quad_graph: nx.Graph
+) -> None:
     state = RoutingState(
         max_interaction_gates=max_interaction_gates,
         max_observation_reach=max_observation_reach,
@@ -33,83 +48,86 @@ def test_routing_state_initialize(
         observation_connection_flag=False,
     )
     assert state.max_interaction_gates == max_interaction_gates
+    expected = min(max_interaction_gates, max_observation_reach)
+    assert state.max_observation_reach == expected
     assert state.swap_gates_inserted == []
     assert state.position == 0
-    assert state.max_observation_reach <= int(
-        min(max_observation_reach, len(state.interaction_circuit))
-    )
-    assert len(state.interaction_circuit) >= 0
+    assert state.steps_done == 0
+    circuit = np.asarray(state.interaction_circuit)
+    assert circuit.shape[1] == 2
+    assert (circuit < state.n_qubits).all()
+    assert state.connection_graph is quad_graph
 
 
-@pytest.fixture
-def quad_graph():
-    quad_graph = nx.Graph()
-    quad_graph.add_edge(0, 1)
-    quad_graph.add_edge(1, 2)
-    quad_graph.add_edge(2, 3)
-    quad_graph.add_edge(3, 0)
-    return quad_graph
-
-
-@pytest.mark.parametrize(
-    "max_interaction_gates, max_observation_reach",
-    [(1, 1), (5, 5), (10, 5), (50, 5), (5, 10)],
-)
-def test_routing_state(max_interaction_gates, max_observation_reach, quad_graph):
-    state = RoutingState(
-        max_interaction_gates=max_interaction_gates,
-        max_observation_reach=max_observation_reach,
-        connection_graph=quad_graph,
-        observation_booleans_flag=False,
-        observation_connection_flag=False,
-    )
-    # test observation typing
-    observation_space = state.create_observation_space()
+def test_create_observation_space(simple_state: RoutingState) -> None:
+    observation_space = simple_state.create_observation_space()
     assert isinstance(observation_space, qgym.spaces.Dict)
     assert isinstance(
         observation_space["interaction_gates_ahead"], qgym.spaces.MultiDiscrete
     )
     assert isinstance(observation_space["current_mapping"], qgym.spaces.MultiDiscrete)
 
-    # test interaction circuit properties
-    assert len(state.interaction_circuit) <= max_interaction_gates
-    assert isinstance(state.interaction_circuit, list)
 
-    # test can_be_executed for the given connection graph
-    assert state._can_be_executed(3, 0) and state._can_be_executed(0, 3)
-    assert state._can_be_executed(0, 1) and state._can_be_executed(1, 0)
-    assert state._can_be_executed(1, 2) and state._can_be_executed(2, 1)
-    assert state._can_be_executed(2, 3) and state._can_be_executed(3, 2)
-    assert (not state._can_be_executed(1, 3)) and (not state._can_be_executed(3, 1))
-    assert (not state._can_be_executed(0, 2)) and (not state._can_be_executed(2, 0))
+def test_interaction_circuit_properties(simple_state: RoutingState) -> None:
+    assert len(simple_state.interaction_circuit) <= simple_state.max_interaction_gates
+    assert isinstance(simple_state.interaction_circuit, list)
 
-    # test obtain_observation
-    assert isinstance(state.obtain_observation()["interaction_gates_ahead"], list)
-    assert isinstance(state.obtain_observation()["current_mapping"], list)
-    assert (
-        len(state.obtain_observation()["interaction_gates_ahead"])
-        >= state.max_observation_reach
+
+class TestCanBeExecuted:
+    @pytest.mark.parametrize("qubit1, qubit2", [(0, 3), (0, 1), (1, 2), (2, 3)])
+    def test_succes(self, simple_state: RoutingState, qubit1: int, qubit2: int) -> None:
+        assert simple_state._can_be_executed(qubit1, qubit2)
+        assert simple_state._can_be_executed(qubit2, qubit1)
+
+    @pytest.mark.parametrize("qubit1, qubit2", [(1, 3), (0, 2)])
+    def test_fail(self, simple_state: RoutingState, qubit1: int, qubit2: int) -> None:
+        assert not simple_state._can_be_executed(qubit1, qubit2)
+        assert not simple_state._can_be_executed(qubit2, qubit1)
+
+
+def test_obtain_observation(simple_state: RoutingState) -> None:
+    observation = simple_state.obtain_observation()
+    observation_space = simple_state.create_observation_space()
+    assert observation in observation_space
+
+
+class TestUpdateState:
+    @pytest.mark.parametrize(
+        argnames="action, expected_mapping",
+        argvalues=[([0, 2, 3], [0, 1, 3, 2]), ([0, 1, 3], [0, 1, 2, 3])],
+        ids=["legal", "illegal"],
     )
-    current_observation_reach = state.observation_reach
+    def test_swap(
+        self,
+        simple_state: RoutingState,
+        action: NDArray[np.int_],
+        expected_mapping: ArrayLike,
+    ) -> None:
+        simple_state.update_state(np.asarray(action))
+        assert simple_state.position == 0
+        assert simple_state.steps_done == 1
+        assert np.array_equal(simple_state.current_mapping, expected_mapping)
 
-    # test update_state
-    #   swapping
-    state.update_state((0, 2, 3))
-    assert state.current_mapping[2] == 3
-    assert state.current_mapping[3] == 2
-    assert state._can_be_executed(2, 0) and state._can_be_executed(0, 2)
-    assert not (state._can_be_executed(3, 0) and state._can_be_executed(0, 3))
-    state.update_state((0, 3, 0))
-    assert state.current_mapping[2] == 3
-
-    #   surpassing
-    flag = state.observation_reach == len(
-        state.obtain_observation()["interaction_gates_ahead"]
+    @pytest.mark.parametrize(
+        argnames="interaction_circuit, expected_position",
+        argvalues=[([(0, 1)], 1), ([(0, 2)], 0)],
+        ids=["legal", "illegal"],
     )
-    state.update_state((1, 2, 9))
-    if flag:
-        state.observation_reach == current_observation_reach - 1
+    def test_surpass(
+        self,
+        simple_state: RoutingState,
+        interaction_circuit: List[Tuple[int, int]],
+        expected_position: int,
+    ) -> None:
+        simple_state.interaction_circuit = interaction_circuit
+        simple_state.update_state(np.array([1, 10, 10]))
+        assert simple_state.position == expected_position
+        assert simple_state.steps_done == 1
+        assert np.array_equal(simple_state.current_mapping, [0, 1, 2, 3])
 
-    # test reset
-    state.reset()
-    assert [state.current_mapping[idx] == idx for idx in range(state.n_qubits)]
+
+def test_reset(simple_state: RoutingState) -> None:
+    assert isinstance(simple_state.reset(), RoutingState)
+    assert simple_state.swap_gates_inserted == []
+    assert simple_state.position == 0
+    assert simple_state.steps_done == 0
