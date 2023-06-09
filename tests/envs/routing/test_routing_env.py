b4e49f315311ad488d7da16eec85daf423593aed
Stan van der Linde <stan.vanderlinde@tno.nl>
2023-06-09 16:57:02 +0200

    More tests

--- tests/envs/routing/test_routing_env.py
+++ tests/envs/routing/test_routing_env.py
@@ -1,14 +1,24 @@
+from typing import Any, Dict
+
 import pytest
 from stable_baselines3.common.env_checker import check_env
 
 from qgym.envs.routing.routing import Routing
 
 
-@pytest.fixture(name="small_env")
-def small_env_fixture() -> Routing:
-    return Routing(connection_grid_size=(2, 2))
-
-
-def test_validity(small_env: Routing) -> None:
-    check_env(small_env, warn=True)  # todo: maybe switch this to the gym env checker
-    assert True
+@pytest.mark.parametrize(
+    "kwargs",
+    [
+        {"connection_grid_size": (2, 2)},
+        {"connection_grid_size": (2, 2), "observation_booleans_flag": False},
+        {"connection_grid_size": (2, 2), "observation_connection_flag": False},
+        {
+            "connection_grid_size": (2, 2),
+            "observation_booleans_flag": False,
+            "observation_connection_flag": False,
+        },
+    ],
+)
+def test_validity(kwargs: Dict[str, Any]) -> None:
+    env = Routing(**kwargs)
+    check_env(env, warn=True)  # todo: maybe switch this to the gym env checker
