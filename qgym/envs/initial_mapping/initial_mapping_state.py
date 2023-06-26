5609471af8302f0f0ced9efb0dcdcb3e494aa5ee
Willem de kok <willem.dekok@tno.nl>
2023-06-26 16:54:00 +0200

    example usage RoutingState

--- qgym/envs/initial_mapping/initial_mapping_state.py
+++ qgym/envs/initial_mapping/initial_mapping_state.py
@@ -7,7 +7,13 @@ Usage:
     >>> from qgym.envs.initial_mapping.initial_mapping_state import InitialMappingState
     >>> import networkx as nx
     >>> connection_graph = nx.grid_graph((3,3))
-    >>> state = InitialMappingState(connection_graph, 0.5)
+    >>> state = RoutingState(
+    >>>             max_interaction_gates = 100,
+    >>>             max_observation_reach = 20,
+    >>>             connection_graph = connection_graph,
+    >>>             observation_booleans_flag = True,
+    >>>             observation_connection_flag = False,
+    >>>             )
 
 """
 from __future__ import annotations
