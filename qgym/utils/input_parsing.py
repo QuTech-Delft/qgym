8d305f7bc374d5c31efabe760f9e3c2000bd4438
Stan van der Linde <stan.vanderlinde@tno.nl>
2023-06-28 10:00:00 +0200

    tests for input_parsing

--- qgym/utils/input_parsing.py
+++ qgym/utils/input_parsing.py
@@ -3,16 +3,24 @@
 With parsing we mean that the user input is validated and transformed to a predictable
 format. In this way, user can give different input formats, but internally we are 
 assured that the data has the same format."""
+import warnings
 from copy import deepcopy
-from typing import Type, Union
+from typing import Iterable, List, Optional, Tuple, Type, Union
+
+import networkx as nx
+from numpy.typing import ArrayLike
 
 from qgym.templates import Rewarder
-from qgym.utils.input_validation import check_instance
+from qgym.utils.input_validation import (
+    check_adjacency_matrix,
+    check_graph_is_valid_topology,
+    check_instance,
+)
+
+Gridspecs = Union[List[Union[int, Iterable[int]]], Tuple[Union[int, Iterable[int]]]]
 
 
-def parse_rewarder(
-    rewarder: Union[Rewarder, None], default: Type[Rewarder]
-) -> Rewarder:
+def parse_rewarder(rewarder: Optional[Rewarder], default: Type[Rewarder]) -> Rewarder:
     """Parse a `rewarder` given by the user.
 
     :param rewarder: ``Rewarder`` to use for the environment. If ``None``, then a new
@@ -26,3 +34,49 @@ def parse_rewarder(
         return default()
     check_instance(rewarder, "rewarder", Rewarder)
     return deepcopy(rewarder)
+
+
+def parse_connection_graph(
+    connection_graph: Optional[nx.Graph] = None,
+    connection_graph_matrix: Optional[ArrayLike] = None,
+    connection_grid_size: Optional[Gridspecs] = None,
+) -> nx.Graph:
+    """Parse the user input (given in ``__init__``) to create a connection graph.
+
+    :param connection_graph: ``networkx.Graph`` representation of the QPU topology.
+    :param connection_graph_matrix: Adjacency matrix representation of the QPU
+        topology
+    :param connection_grid_size: Size of the connection graph when the topology is a
+        grid.
+    :raise ValueError: When `connection_graph`, `connection_graph_matrix` and
+        `connection_grid_size` are all None.
+    :return: Connection graph as a ``networkx.Graph``.
+    """
+    if connection_graph is not None:
+        if connection_graph_matrix is not None:
+            msg = "Both 'connection_graph' and 'connection_graph_matrix' were given. "
+            msg += "Using 'connection_graph'."
+            warnings.warn(msg)
+        if connection_grid_size is not None:
+            msg = "Both 'connection_graph' and 'connection_grid_size' were given. "
+            msg += "Using 'connection_graph'."
+            warnings.warn(msg)
+
+        check_graph_is_valid_topology(connection_graph, "connection_graph")
+
+        # deepcopy the graphs for safety
+        return deepcopy(connection_graph)
+
+    if connection_graph_matrix is not None:
+        if connection_grid_size is not None:
+            msg = "Both 'connection_graph_matrix' and 'connection_grid_size' were "
+            msg += "given. Using 'connection_graph_matrix'."
+            warnings.warn(msg)
+        connection_graph_matrix = check_adjacency_matrix(connection_graph_matrix)
+        return nx.from_numpy_array(connection_graph_matrix)
+    if connection_grid_size is not None:
+        # Generate connection grid graph
+        return nx.grid_graph(connection_grid_size)
+
+    msg = "No valid arguments for instantiation a connection graph were given"
+    raise ValueError(msg)
---
+++ tests/utils/test_input_parsing.py
@@ -0,0 +1,77 @@
+import networkx as nx
+import pytest
+
+from qgym.envs.initial_mapping import BasicRewarder, EpisodeRewarder
+from qgym.utils.input_parsing import parse_connection_graph, parse_rewarder
+
+
+class TestParseRewarder:
+    def test_default(self):
+        output = parse_rewarder(None, BasicRewarder)
+        assert isinstance(output, BasicRewarder)
+        assert output is not BasicRewarder()
+
+    def test_default(self):
+        input_rewarder = EpisodeRewarder()
+        output = parse_rewarder(input_rewarder, BasicRewarder)
+        assert isinstance(output, EpisodeRewarder)
+        # Test deepcopy
+        assert output is not input_rewarder
+
+
+class TestParseConnectionGraph:
+    @pytest.fixture(scope="class", name="expected_output")
+    def expected_output_fixture(self) -> nx.Graph:
+        graph = nx.Graph()
+        graph.add_edge(0, 1)
+        return graph
+
+    def test_parse_connection_graph(self, expected_output: nx.Graph) -> None:
+        graph = nx.Graph()
+        graph.add_edge(0, 1)
+        output_graph = parse_connection_graph(connection_graph=graph)
+        assert nx.is_isomorphic(output_graph, expected_output)
+        # Test if it is a copy
+        assert graph is not output_graph
+
+    def test_parse_connection_graph(self, expected_output: nx.Graph) -> None:
+        output_graph = parse_connection_graph(connection_graph_matrix=[[0, 1], [1, 0]])
+        assert nx.is_isomorphic(output_graph, expected_output)
+
+    def test_parse_connection_graph(self, expected_output: nx.Graph) -> None:
+        output_graph = parse_connection_graph(connection_grid_size=(1, 2))
+        assert nx.is_isomorphic(output_graph, expected_output)
+
+
+class TestParseConnectionGraphWarnings:
+    @pytest.mark.parametrize(
+        "connection_graph_matrix,connection_grid_size",
+        [("test", None), (None, "test"), ("test", "test")],
+    )
+    def test_nx_graph_and_other(self, connection_graph_matrix, connection_grid_size):
+        small_graph = nx.Graph()
+        small_graph.add_edge(0, 1)
+
+        with pytest.warns(UserWarning):
+            parse_connection_graph(
+                connection_graph=small_graph,
+                connection_graph_matrix=connection_graph_matrix,
+                connection_grid_size=connection_grid_size,
+            )
+
+    def test_array_like_and_gridspec(self):
+        with pytest.warns(UserWarning):
+            parse_connection_graph(
+                connection_graph=None,
+                connection_graph_matrix=[[0, 1], [1, 0]],
+                connection_grid_size="test",
+            )
+
+
+def test_parse_connection_graph_exception():
+    with pytest.raises(ValueError):
+        parse_connection_graph(
+            connection_graph=None,
+            connection_graph_matrix=None,
+            connection_grid_size=None,
+        )
