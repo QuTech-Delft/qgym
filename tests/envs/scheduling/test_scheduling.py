0756318da1d867e8f0e3fe3980e9dfefbe18f084
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-03-06 16:17:05 +0100

    cleanup tests

--- tests/envs/scheduling/test_scheduling.py
+++ tests/envs/scheduling/test_scheduling.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Collection
+from collections.abc import Collection
+from typing import TYPE_CHECKING
 
 import numpy as np
 import pytest
