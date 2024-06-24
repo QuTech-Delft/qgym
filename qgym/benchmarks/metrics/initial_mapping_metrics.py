ead31e8da47788f2379326fd3ae2651222e2363c
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-06-24 10:18:42 +0200

    addd tests and docstrings

--- qgym/benchmarks/metrics/initial_mapping_metrics.py
+++ qgym/benchmarks/metrics/initial_mapping_metrics.py
@@ -11,6 +11,7 @@ from __future__ import annotations
 
 from collections import deque
 from collections.abc import Iterable
+from copy import deepcopy
 from typing import Literal, Protocol, runtime_checkable
 
 import networkx as nx
@@ -50,6 +51,8 @@ class DistanceRatioLoss(InitialMappingMetric):
                 self.distance_matrix[node_u, node_v] = distances[node_v]
 
     def compute(self, interaction_graph: nx.Graph, mapping: ArrayLike) -> float:
+        if interaction_graph.number_of_edges() == 0:
+            return 1.0
         mapping = np.asarray(mapping, dtype=np.int_)
         distance_loss = 0
         for edge in interaction_graph.edges():
@@ -73,10 +76,26 @@ class AgentPerformance:
 class InitialMappingBenchmarker:
 
     def __init__(
-        self, generator: GraphGenerator | None, metrics: Iterable[InitialMappingMetric]
+        self,
+        generator: GraphGenerator | None = None,
+        *,
+        metrics: Iterable[InitialMappingMetric] | InitialMappingMetric,
     ) -> None:
+        """Init of the :clas:`InitialMappingBenchmarker` class.
+
+        Args:
+            generator: Interaction graph generator to use during benchmarking
+            metrics: Metrics to compute.
+        """
         self.generator = BasicGraphGenerator() if generator is None else generator
-        self.metrics = tuple(metrics)
+        self.metrics = (
+            (metrics,) if isinstance(metrics, InitialMappingMetric) else tuple(metrics)
+        )
+        for metric in self.metrics:
+            if hasattr(metric, "connection_graph"):
+                connection_graph = deepcopy(metric.connection_graph)
+                self.generator.set_state_attributes(connection_graph=connection_graph)
+                break
 
     def run(
         self,
@@ -84,11 +103,20 @@ class InitialMappingBenchmarker:
         max_iter: int = 1000,
         return_type: Literal["raw", "quartiles", "median", "mean"] = "raw",
     ) -> NDArray[np.float_]:
-        self.generator.set_state_attributes
+        """Run the benchmark.
+
+        Args:
+            mapper: Mapper to benchmark.
+            max_iter: Maximum number of iterations to benchmark.
+            return_type: Return type to return the results.
+
+        Returns:
+            NDArray containing the results from the benchmark.
+        """
         return_type = check_string(return_type, "return_type", lower=True)
 
         results = [deque() for _ in self.metrics]
-        for i, interaction_graph in enumerate(self.generator):
+        for i, interaction_graph in enumerate(self.generator, start=1):
             mapping = mapper.compute_mapping(interaction_graph)
             for metric, result_que in zip(self.metrics, results):
                 result_que.append(metric.compute(interaction_graph, mapping))
@@ -104,12 +132,3 @@ class InitialMappingBenchmarker:
             return np.median(results, axis=1)
         if return_type == "mean":
             return np.mean(results, axis=1)
-
-
-if __name__ == "__main__":
-    metric = DistanceRatioLoss(nx.cycle_graph(4))
-
-    print(isinstance(metric, InitialMappingMetric))
-    print(metric.compute(nx.complete_graph(4), np.arange(4)))
-
-    benchmarker = InitialMappingBenchmarker()
