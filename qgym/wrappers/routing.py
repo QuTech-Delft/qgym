a0a09fcb1b672becb57de4a21e32e9ee8fb125e5
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-11-27 14:21:28 +0100

    First attempt swap adder

--- qgym/wrappers/routing.py
+++ qgym/wrappers/routing.py
@@ -29,7 +29,11 @@ class AgentRoutingWrapper(  # pylint: disable=too-few-public-methods
     required for the qgym benchmarking tools.
     """
 
+<<<<<<< HEAD
     def __init__(  # pylint: disable=useless-parent-delegation
+=======
+    def __init__(
+>>>>>>> 714f32c (First attempt swap adder)
         self,
         agent: BaseAlgorithm,
         env: Routing,
@@ -50,6 +54,7 @@ class AgentRoutingWrapper(  # pylint: disable=too-few-public-methods
                 and the `predict` method of `agent` should accept the keyword argument
                 `"action_masks"`. If ``False`` (default) no action masking is used.
         """
+<<<<<<< HEAD
         super().__init__(agent, env, max_steps, use_action_masking=use_action_masking)
 
     def _prepare_episode(self, circuit: Circuit) -> dict[str, NDArray[np.int_]]:
@@ -70,6 +75,18 @@ class AgentRoutingWrapper(  # pylint: disable=too-few-public-methods
 
     def compute_routing(self, circuit: CircuitLike) -> Circuit:
         """Route the `circuit` using the provided `agent` and `env`.
+=======
+        self.agent = agent
+        self.env = env
+        self.max_steps = max_steps
+        self.use_action_masking = use_action_masking
+        if self.use_action_masking and not hasattr(self.env, "action_masks"):
+            msg = "use_action_mask is True, but env has no action_masks attribute"
+            raise TypeError(msg)
+
+    def compute_routing(self, circuit: QuantumCircuit | DAGCircuit) -> NDArray[np.int_]:
+        """Compute a mapping of the `circuit` using the provided `agent` and `env`.
+>>>>>>> 714f32c (First attempt swap adder)
 
         Args:
             circuit: Quantum circuit to route. The circuit must be a physical circuit,
@@ -80,6 +97,7 @@ class AgentRoutingWrapper(  # pylint: disable=too-few-public-methods
             Routed circuit, i.e. a quantum circuit that only contains two qubit gates
             between qubits that are part of the connection graph.
         """
+<<<<<<< HEAD
         return self.run(circuit)
 
 
@@ -114,3 +132,68 @@ class QiskitRoutingWrapper:
     def __repr__(self) -> str:
         """String representation of the wrapper."""
         return f"{self.__class__.__name__}[{self.routing}]"
+=======
+        interaction_circuit = get_interaction_circuit(circuit)
+        obs, _ = self.env.reset(options={"interaction_circuit": interaction_circuit})
+
+        predict_kwargs = {"observation": obs}
+        for _ in range(self.max_steps):
+            if self.use_action_masking:
+                action_masks = self.env.action_masks()  # type: ignore[attr-defined]
+                predict_kwargs["action_masks"] = action_masks
+
+            action, _ = self.agent.predict(**predict_kwargs)
+            predict_kwargs["observation"], _, done, _, _ = self.env.step(action)
+            if done:
+                break
+
+        if not done:
+            msg = (
+                "routing not found, "
+                "the episode was truncated or 'max_steps' was reached"
+            )
+            raise ValueError(msg)
+
+        return add_swaps_to_circuit(circuit, self.env._state.swap_gates_inserted)
+
+
+class QiskitMapperWrapper:
+    """Wrap any qiskit mapper (:class:`~qiskit.transpiler.Layout`) such that it becomes
+    compatible with the qgym framework. This class wraps the qiskit mapper, such that it
+    is compatible with the qgym Mapper protocol, which is required for the qgym
+    benchmarking tools.
+    """
+
+    def __init__(self, qiskit_mapper: Layout) -> None:
+        """Init of the :class:`QiskitMapperWrapper`.
+
+        Args:
+            qiskit_mapper: The qiskit mapper (:class:`~qiskit.transpiler.Layout`) to
+                wrap.
+        """
+        self.mapper = qiskit_mapper
+
+    def compute_mapping(self, circuit: QuantumCircuit | DAGCircuit) -> NDArray[np.int_]:
+        """Compute a mapping of the `circuit` using the provided `qiskit_mapper`.
+
+        Args:
+            circuit: Quantum circuit to map.
+
+        Returns:
+            Array of which the index represents a physical qubit, and the value a
+            virtual qubit.
+        """
+        dag = parse_circuit(circuit)
+
+        self.mapper.run(dag)
+        layout = self.mapper.property_set["layout"]
+
+        # Convert qiskit layout to qgym mapping
+        qreg_to_int = _get_qreg_to_int_mapping(dag)
+        iterable = (qreg_to_int[layout[i]] for i in range(dag.num_qubits()))
+        return np.fromiter(iterable, int, dag.num_qubits())
+
+    def __repr__(self) -> str:
+        """String representation of the wrapper."""
+        return f"{self.__class__.__name__}[{self.mapper}]"
+>>>>>>> 714f32c (First attempt swap adder)
