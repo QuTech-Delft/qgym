70511362e99ae459aadd92d768bc8292eb2041a3
Stan van der Linde <stan.vanderlinde@tno.nl>
2024-12-19 23:04:18 +0100

    Make Circuit and CircuitLike as benchmark input

--- tests/benchmarks/metrics/test_routing_metrics.py
+++ tests/benchmarks/metrics/test_routing_metrics.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import numpy as np
 import pytest
 from qiskit import QuantumCircuit
-from qiskit.dagcircuit import DAGCircuit
 
 from qgym.benchmarks import BenchmarkResult
 from qgym.benchmarks.metrics import (
@@ -12,7 +11,7 @@ from qgym.benchmarks.metrics import (
     RoutingMetric,
 )
 from qgym.generators import MaxCutQAOAGenerator
-from qgym.utils.qiskit_utils import parse_circuit
+from qgym.utils import Circuit, CircuitLike
 
 from qgym.benchmarks import BenchmarkResult
 from qgym.benchmarks.metrics import (
